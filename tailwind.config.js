615b5bb63ed2f16cd9e171bdd0f002850cea26c4
José Neto <joseneto1500@gmail.com>
2024-01-19 21:50:37 -0300

    style input component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        input: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+      },
+      colors: {
+        clifford: '#da373d',
+      }
+    },
   },
   plugins: [],
 }
