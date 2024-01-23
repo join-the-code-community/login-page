5de96bac692f066215dcf98ab75dadc65b71c471
GersonAguiargg <gersonsilvaaguiar7@gmail.com>
2024-01-22 22:49:59 -0300

    ajustando ifs

--- sining.js
+++ sining.js
@@ -1,24 +1,19 @@
 const submitButtonLogin = document.querySelector("#submitButtonValue");
-const emailInput = document.querySelector("#emailValue");
-const passwordInput = document.querySelector("#passwordValue");
+const emailInput = document.querySelector("#emailValue").value;
+const passwordInput = document.querySelector("#passwordValue").value;
 
 const userInfo = JSON.parse(localStorage.getItem("userResgisterd"));
 
 submitButtonLogin.addEventListener("click", () => {
   const { email, passWordValue } = userInfo;
+  const verifyHasValue = emailInput.length > 1 && passwordInput.length > 1;
+  const verifyIfIsCorrect =
+    emailInput.value === email && passwordInput.value === passWordValue;
 
-  if (emailInput.value.length > 1 && passwordInput.value.length > 1) {
-    if (emailInput.value === email) {
-      if (passwordInput.value === passWordValue) {
-        window.location.href = "http://127.0.0.1:5500/Creditos.html";
-
-        return;
-      }
-    } else {
-      alert("senha errada");
-    }
-  } else {
-    alert("Digite algo");
+  if (verifyHasValue) {
+    verifyIfIsCorrect
+      ? (window.location.href = "http://127.0.0.1:5500/Creditos.html")
+      : alert("Senha ou email inválidos");
   }
+  return alert("Digite algo");
 });
-
